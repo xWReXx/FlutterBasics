7e15aca26b4fdbe75a3b064f387c62a93ebf40fb
xwrexx <wrex1285@gmail.com>
2019-08-25 00:59:42 -0600

    created first stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,34 @@ import 'package:flutter/material.dart';
 
 void main()=> runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-void answerQuestions() {
-  print('Answer Chosen!');
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
 }
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  
+  void answerQuestions() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print('Answer Chosen!');
+  }
 
   @override
   Widget build(BuildContext context) {
-    // var questions = ['who?', 'what?', 'why?'];
+    var questions = ['who?', 'what?', 'why?'];
     return MaterialApp(
       home: Scaffold (
         appBar: AppBar(title: Text('My First App'),
         ),
         body: Column (
           children: [
-            Text('The Question!'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestions,
