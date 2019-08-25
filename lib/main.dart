21acee51f3c0e0fb345f88c27b3fdb0052391ba7
xwrexx <wrex1285@gmail.com>
2019-08-25 00:41:19 -0600

    added basic buttons and coloumns to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
+// void main(){
+//   runApp(MyApp());
+// }
+
+void main()=> runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+void answerQuestions() {
+  print('Answer Chosen!');
+}
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello'),);
+    // var questions = ['who?', 'what?', 'why?'];
+    return MaterialApp(
+      home: Scaffold (
+        appBar: AppBar(title: Text('My First App'),
+        ),
+        body: Column (
+          children: [
+            Text('The Question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestions,
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestions,
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestions,
+            ),
+          ]
+        ),
+      )
+    );
   }
 }
